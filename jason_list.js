697943bb071f3be0e0f046b209eb13ae8af49934
lilasilva <94986224+lilasilva@users.noreply.github.com>
2021-11-26 13:58:00 -0300

    Add files via upload

---
+++ jason_list.js
@@ -0,0 +1,51 @@
+
+var listModel = [
+      {
+            img: 'https://img.xatblog.net/image/SLVgnMLAd.png',
+            desc: 'Fundo Interno - guerreiros'
+      },
+      {
+            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
+            desc: 'Fundo Externo - Carinho e Amizade'
+      },
+      {
+            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
+            desc: 'Fundo Interno - Hola'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Hola'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
+      },
+      {
+            img: 'https://i.postimg.cc/FHPxBzwb/inner-radio0120-bg.jpg',
+            desc: 'Fundo Interno - Cafafo'
+      },
+      {
+            img: 'https://i.postimg.cc/69jHHbXs/radio0120-bg.jpg',
+            desc: 'Fundo Externo - Carinho e Amizade'
+      },
+      {
+            img: 'https://i.postimg.cc/ncmtghfD/inner-hola-01.jpg',
+            desc: 'Fundo Interno - Hola'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Hola'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
+      },
+      {
+            img: 'https://i.postimg.cc/HkCCg1TP/hola-bg-01.jpg',
+            desc: 'Fundo Externo - Monicomio'
+      },
+]
